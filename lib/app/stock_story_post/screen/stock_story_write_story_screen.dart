6f10098d167f9bc50617ba2adc0a85f40b90b34f
하지훈 <jnh03336@gmail.com>
2023-12-07 12:54:27 +0700

    feat: add textfield on stock story write screen

--- lib/app/stock_story_post/screen/stock_story_write_story_screen.dart
+++ lib/app/stock_story_post/screen/stock_story_write_story_screen.dart
@@ -9,7 +9,22 @@ class StockStoryWriteStoryScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('주식 이야기 등록'),
       ),
-      body: Column(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          children: const [
+            TextField(
+              style: TextStyle(fontSize: 18),
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: '주식 구매/판매 하게된 이야기를 적어주세요.',
+              ),
+              minLines: 12,
+              maxLines: 12,
+            ),
+          ],
+        ),
+      ),
       bottomSheet: Container(
         height: 70,
         decoration: const BoxDecoration(
